f83e2103a104a4fddccdafcad737907994caa692
Vic Scherman <vicscherman@gmail.com>
2022-02-01 13:53:14 -0500

    added thunk, put shop data/request in reducer instead of component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,6 @@
 import { createSelector } from "reselect";
 import memoize from "lodash.memoize";
 
-
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -12,13 +10,17 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => collections ? Object.keys(collections).map(key => collections[key]) :[]
-)
-
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
+);
 
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections? collections[collectionUrlParam] : null
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   )
+);
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
 );
